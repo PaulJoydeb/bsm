ad8d96bc11579bbb3e997757232800593e3689f4
sharifulkawsar <mdsharifulkawsar@gmail.com>
2023-03-12 00:10:44 +0600

    dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\BuyBook;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,9 +11,10 @@ class AdminController extends Controller
     public function index()
     {
         $total_books = Book::count();
-        $buy_books = 0;
-        $deliver_pending = 0;
-        $pending_request = 0;
+        $buy_book = new BuyBook();
+        $buy_books = $buy_book->where('process', 3)->count();;
+        $deliver_pending = $buy_book->where('process', 2)->count();;
+        $pending_request = $buy_book->where('process', 1)->count();
         return view('admin', compact('total_books', 'buy_books', 'deliver_pending', 'pending_request'));
     }
 }
